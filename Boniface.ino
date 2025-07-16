b62112caebf2261a57fa442570f84e0b29f9c305
Boniface Saaburo <65026605+Boniface93@users.noreply.github.com>
2025-07-16 21:06:00 +0000

    Add files via upload

---
+++ Boniface.ino
@@ -0,0 +1,23 @@
+
+// Define the analog pin
+const int potPin = 12;
+
+// GPI012 is input only
+void
+setup() {
+Serial. begin(115200);
+}
+
+// Start Serial Monitor at 115200 baud rate
+void
+loop () {
+int potValue = analogRead(potPin);
+
+// Read analog value (0-4095)
+Serial.print( "Potentiometer Value: ");
+Serial.println(potValue);
+
+// Print the value
+delay lay(200) ;
+// Delay for readability
+}
